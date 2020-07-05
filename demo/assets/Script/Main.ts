bd80263863779c47363914d31df8349819c4396d
ankye <shengxiehua@gmail.com>
2020-07-05 21:43:10 +0800

    添加loading页

--- demo/assets/Script/Main.ts
+++ demo/assets/Script/Main.ts
@@ -2,10 +2,27 @@ const { ccclass, property } = cc._decorator;
 
 @ccclass
 export default class Main extends cc.Component {
+    private _view: fgui.GComponent;
+
     onLoad() {
         fgui.addLoadHandler();
         fgui.GRoot.create();
         airkit.Framework.Instance.setup(fgui.GRoot.inst);
+
+        //这里填写的是相对于resources里的路径
+        let res = [
+            "ui/Loader", //描述文件
+            "ui/Loader_atlas0", //纹理集
+        ];
+        cc.loader.loadResArray(res, function (err, assets) {
+            //都加载完毕后再调用addPackage
+            fgui.UIPackage.addPackage("ui/Loader");
+
+            //下面就可以开始创建包里的界面了。
+            this._view = fgui.UIPackage.createObject("Loader", "Login").asCom;
+            this._view.makeFullScreen();
+            fgui.GRoot.inst.addChild(this._view);
+        });
     }
 
     start() {
